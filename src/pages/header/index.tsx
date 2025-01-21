17a26ccf025523437c3efba8333f6245dbd967f1
João Lopes <joaovlopesmartins@gmail.com>
2025-01-20 23:36:50 -0300

    feat: create header component

--- src/pages/header/index.tsx
+++ src/pages/header/index.tsx
@@ -1,7 +1,16 @@
+import Logo  from '@/assets/logo.png'
+import './styles.scss';
+
 export function Header() {
     return (
-        <>
-            <h1>Header</h1>
-        </>
+        <header className='header-container'>
+            <button>
+                <i className="fa-solid fa-bars"></i>
+            </button>
+            <img src={Logo} alt="Logo da empresa" className='logo-image'/>
+            <button>
+                <i className="fa-solid fa-circle-user"></i>
+            </button>
+        </header>
     )
 }
